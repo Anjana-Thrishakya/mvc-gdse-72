79c8d3522b7fccbf8d76e89334574486a1bd0cbf
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-08-22 13:18:24 +0530

    Item DTO Creation

---
+++ src/edu/ijse/mvc/dto/ItemDto.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.dto;
+
+/**
+ *
+ * @author anjan
+ */
+public class ItemDto {
+    private String code;
+    private String description;
+    private String pack;
+    private double unitPrice;
+    private int qoh;
+
+    public ItemDto() {
+    }
+
+    public ItemDto(String code, String description, String pack, double unitPrice, int qoh) {
+        this.code = code;
+        this.description = description;
+        this.pack = pack;
+        this.unitPrice = unitPrice;
+        this.qoh = qoh;
+    }
+
+    /**
+     * @return the code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the pack
+     */
+    public String getPack() {
+        return pack;
+    }
+
+    /**
+     * @param pack the pack to set
+     */
+    public void setPack(String pack) {
+        this.pack = pack;
+    }
+
+    /**
+     * @return the unitPrice
+     */
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    /**
+     * @param unitPrice the unitPrice to set
+     */
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    /**
+     * @return the qoh
+     */
+    public int getQoh() {
+        return qoh;
+    }
+
+    /**
+     * @param qoh the qoh to set
+     */
+    public void setQoh(int qoh) {
+        this.qoh = qoh;
+    }
+
+    
+}
